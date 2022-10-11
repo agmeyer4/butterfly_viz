cabfc5c287623178225cae881694f61bf5d2f46c
Aaron Meyer <agmeyer4@gmail.com>
2022-10-10 22:39:40 -0600

    tph and spectral test

--- application.py
+++ application.py
@@ -15,8 +15,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 #declare the app
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-s3 = boto3.client('s3')
-
+#s3 = boto3.client('s3')
 
 ########### Set up the layout
 app.layout = html.Div(children=[
@@ -24,28 +23,54 @@ app.layout = html.Div(children=[
     dcc.Graph(
             id='plot1',
         ),
+    dcc.Graph(
+            id='plot2',
+        ),
 	dcc.Interval(id = 'interval-component',
 						interval = 1*5000,#milliseconds
 						n_intervals = 0
 					),
 ])
 
-
 @app.callback(
     Output('plot1', 'figure'),
 	Input('interval-component','n_intervals')
 )
 def update_plot1(df):
-	bucket='carbonveda-poc-test'
-	key='spectra_09-28-39.txt'
-	data = builder._s3_np(s3,bucket,key)
+	#bucket='carbonveda-poc-test'
+	#key='spectra_09-28-39.txt'
+	#data = builder._s3_np(s3,bucket,key)
+	data = np.loadtxt(r'C:\Users\agmey\Desktop\spectra_11-37-08_test.txt')
+	spectra = data[5:]
+	ET = data[0]
+	print(pd.to_datetime(ET,unit='s'))
 
 	fig = make_subplots()
 	fig.add_trace(
-		go.Scatter(x = np.arange(0,len(data)), y = data),
+		go.Scatter(x = np.arange(0,len(spectra)), y = spectra),
+	)
+	return fig
+
+@app.callback(
+    Output('plot2', 'figure'),
+	Input('interval-component','n_intervals')
+)
+def update_plot1(df):
+	tph = pd.read_csv('C:/Users/agmey/Desktop/tph_11-37-08.txt',delimiter=' ',header=None).iloc[5:]
+	tph.rename(columns={0:'ET',1:'T',2:'P',3:'RH'},inplace = True)
+	tph['DT'] = pd.to_datetime(tph['ET'],unit='s')
+
+	fig = make_subplots(specs=[[{"secondary_y": True}]])
+	fig.add_trace(
+		go.Scatter(x = tph['DT'], y = tph['T']),
+		secondary_y = False
+	)
+	fig.add_trace(
+		go.Scatter(x = tph['DT'], y = tph['P']),
+		secondary_y=True
 	)
 	return fig
 ########### Run the app
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port=80,debug=True)
-    #app.run_server(debug=True)
+    #app.run_server(host="0.0.0.0", port=80,debug=True)
+    app.run_server(debug=True)
